0190e22a19f0e3d5e2a57caeaea4be9abe22d475
Yash Shah <55008416+yash3108@users.noreply.github.com>
2021-04-10 15:36:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 import pickle
 import cv2
 import numpy as np
-from keras.models import load_model
+# from keras.models import load_model
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
